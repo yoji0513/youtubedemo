393cc08d176ec860d66d8cae0c5ee49b4f7dd81b
takahashiyoji <takahashiyoji@takahashiyojinoMacBook-Pro-2.local>
2023-01-12 15:04:56 +0900

    Youtubedemo完成

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const id = document.getElementById('id')
+const login = () => {
+  const idValue = id.value
+  console.log(idValue)
+}
+if(id){
+  document.getElementById('button').addEventListener('click', () => login());
+}
+
+
+const getYouTubeData = () => {
+  return $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: 'AIzaSyC5jX1GMVmJzKGHYBilpohaQsljfQvBCYU',
+      channelId: 'UCoFLB_Gw_AoxUuuzKjXrc_Q',
+      maxResults: 5,
+      order: 'date',
+    },
+    error: () => {
+      console.error('YouTube動画情報を取得できませんでした。');
+    }
+  });
+}
+
+getYouTubeData().done(function(data) {
+  const items = data.items;
+  const html = [];
+  items.forEach(function(item) {
+    const videoId = item.id.videoId;
+    html.push(`
+      <li>
+        <iframe width="280" height="157" src="https://www.youtube.com/embed/${videoId}"></iframe>
+      </li>
+    `);
+  });
+  $('#result').html(html.join(''));
+});
+
